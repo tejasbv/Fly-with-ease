009c8b799cf91d4b46f2498e1cbf5f056a05446c
tejas <34074610+tejasbv@users.noreply.github.com>
2020-02-25 18:01:32 -0600

    Creating the flutter app with api call

---
+++ Fly with ease/fly_with_ease/lib/apicaller.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:flutter/widgets.dart';
+import 'package:http/http.dart' as http;
+
+class Apicaller {
+  Apicaller() {
+    getInfo();
+  }
+
+  String getInfo() {
+    var thing = fetchAero();
+    thing.then((aero) {
+      print("hello");
+    });
+  }
+
+  String url =
+      "https://aerodatabox.p.rapidapi.com/flights/DL47/2019-08-29?withLocation=false&withAircraftImage=false";
+  Future<Aero> fetchAero() async {
+    final response = await http.get(url, headers: {
+      "x-rapidapi-host": "aerodatabox.p.rapidapi.com",
+      "x-rapidapi-key": "4a4e824657mshab2b5ac6d6b10cbp144f5bjsn0679b5b8ef15"
+    });
+
+    if (response.statusCode == 200) {
+      return Aero.fromJson(json.decode(response.body));
+    } else {
+      throw Exception("api call invalid");
+    }
+  }
+}
+
+class Aero {
+  var Arrival;
+  var departure;
+  Aero(this.Arrival, this.departure);
+  factory Aero.fromJson(List<dynamic> json) {
+    return (Aero(
+      json[0]['arrival'],
+      json[0]['departure'],
+    ));
+  }
+  toString() {
+    return Arrival.toString();
+  }
+}
---
+++ Fly with ease/fly_with_ease/lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:fly_with_ease/apicaller.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  
+
+  @override
+  State<MyHomePage> createState() {
+    return new MyHomePageState();
+  }
+}
+
+class MyHomePageState extends State<MyHomePage> {
+
+  Apicaller caller = new Apicaller();
+  Aero _data;
+
+  @override
+  void initState() {
+    super.initState();
+    caller.fetchAero().then((data) {
+      _data = data;
+      // setState(() {});
+    });
+  }
+
+   @override
+  Widget build(BuildContext context) {
+    
+    // if(data==null)
+    // {
+    //   call();
+    //   return Scaffold(
+    //     appBar: AppBar(
+    //       title: data==null?Text("peep"):Text("app"),
+    //     ));
+    // }
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("app"),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Card(
+              child: Text("top"),
+            ),
+            Text(_data == null ? "TEST" : _data.departure["actualTimeLocal"] + "")
+            
+            // Container(child: Data(), height: 100,)
+          ],
+        ));
+  }
+}
